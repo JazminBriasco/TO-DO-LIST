c90f6492edd9c25e74d59ce996345decdd758fa5
JazBri <jaabris@hotmail.com>
2021-04-11 12:05:30 -0300

    before two modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import FormCard from './components/Form-card';
 import Card from './components/Card';
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function Nav(){
         <>
             <nav className="back-nav">
                 <ul className="ul-nav">
-                    <a href="home" className="li-nac icon-nav"></a> 
+                    <a href="home" className="li-nac icon-nav"> </a> 
                     <a href="all" className="li-nav"><li >ALL</li></a> 
                     <a href="todo" className="li-nav"><li >TO DO</li></a> 
                     <a href="doing" className="li-nav"><li >DOING</li></a>
